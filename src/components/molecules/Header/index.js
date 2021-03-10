b2bea5c8db6a8e55ac46a2a1bb1555685285d1ee
apipshato <afifshatos@gmail.com>
2021-03-10 15:31:42 +0700

    add sign in , custom page sign in

--- src/components/molecules/Header/index.js
+++ src/components/molecules/Header/index.js
@@ -1,14 +1,24 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
 
 const Header = () => {
-    return (
-        <View>
-            <Text>Header Components</Text>
-        </View>
-    )
-}
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Sign In</Text>
+      <Text style={styles.subtitle}>Find your best ever meal</Text>
+    </View>
+  );
+};
 
-export default Header
+export default Header;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'white',
+    paddingHorizontal: 24,
+    paddingTop: 30,
+    paddingBottom: 24,
+  },
+  title: {fontSize: 22, fontFamily: 'Poppins-Medium', color: '#020202'},
+  subtitle: {fontSize: 14, fontFamily: 'Poppins-Light', color: '#8092A3'},
+});
--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,16 +1,30 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
-import { Header } from '../../components'
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import {Button, Header, TextInput} from '../../components';
 
 const SignIn = () => {
-    return (
-        <View>
-        <Header/>
-            <Text>Sign in page</Text>
-        </View>
-    )
-}
+  return (
+    <View style={styles.page}>
+      <Header />
+      <View style={styles.container}>
+        <TextInput />
+        <TextInput />
+        <Button />
+        <Button />
+      </View>
+    </View>
+  );
+};
 
-export default SignIn
+export default SignIn;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  page: {flex: 1},
+  container: {
+    backgroundColor: 'white',
+    paddingHorizontal: 24,
+    paddingVertical: 26,
+    marginTop: 24,
+    flex: 1,
+  },
+});
