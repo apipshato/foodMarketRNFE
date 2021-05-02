7cd17868748b3031062df1b274a50ae3dd584c28
apipshato <afifshatos@gmail.com>
2021-05-02 15:11:53 +0700

    Update index.js

--- src/utils/useForm/index.js
+++ src/utils/useForm/index.js
@@ -1,14 +1,14 @@
 import { useState } from "react";
 
-const useForm = initialValue => {
-  const [form, setForm] = useState(inititalValue);
+const useForm = (initialValue) => {
+  const [form, setForm] = useState(initialValue);
   return [
     form,
     (formType, formValue) => {
       if (formType === "reset") {
         return setForm(initialValue);
       }
-      return setForm({ ...form, [formType]: formValue }); //isi dengan apa form value
+      return setForm({...form, [formType]: formValue }); //isi dengan apa form value
     }
   ];
 };
