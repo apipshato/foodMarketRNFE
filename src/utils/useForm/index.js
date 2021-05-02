b8cf3b6253af62faeaedff3f08cd696252680de6
apipshato <afifshatos@gmail.com>
2021-05-02 14:49:06 +0700

    add utils custom hook

--- src/utils/useForm/index.js
+++ src/utils/useForm/index.js
@@ -1,8 +1,15 @@
-import { useState } from "react"
+import { useState } from "react";
 
-const useForm =(initialValue) =>{
-    const [form, setForm]=useState(inititalValue);
-    return [form, ()=>{
-
-    }]
-}
+const useForm = initialValue => {
+  const [form, setForm] = useState(inititalValue);
+  return [
+    form,
+    (formType, formValue) => {
+      if (formType === "reset") {
+        return setForm(initialValue);
+      }
+      return setForm({ ...form, [formType]: formValue }); //isi dengan apa form value
+    }
+  ];
+};
+export default useForm;
