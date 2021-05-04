401e5cd7a2fd7017926fe053d83a6ed8c886a2f0
apipshato <afifshatos@gmail.com>
2021-05-04 09:26:57 +0700

    reducer

---
+++ src/redux/reducer/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from "redux";
+import authReducer from './auth'
+import globalReducer from './global'
+
+const reducer= combineReducers({authReducer,globalReducer})
+
+
+export default reducer;
