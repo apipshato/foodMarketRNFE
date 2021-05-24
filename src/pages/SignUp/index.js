42fc320917179814ec8b12b22c017f391a52b0bf
apipshato <afifshatos@gmail.com>
2021-05-25 02:21:36 +0700

    Update index.js

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,14 +1,24 @@
-import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
-import {Header, TextInput, Gap, Button} from '../../components';
-import {useSelector} from 'react-redux'
+import React from "react";
+import { ScrollView, StyleSheet, Text, View } from "react-native";
+import { Header, TextInput, Gap, Button } from "../../components";
+import { useSelector } from "react-redux";
+import useForm from "../../utils/useForm";
 
 
-const SignUp = ({navigation}) => {
-  const globalState=useSelector((state) => state.globalReducer);
-  console.log('global', globalState);
+const SignUp = ({ navigation }) => {
+  const [form, setForm] = useForm({
+    name:'',
+    email: '',
+    password:''
+  })
+
+  const onSubmit = () =>{
+    console.log('form: ', form);
+    //navigation.navigate('SignUpAddress')
+  }
   return (
-    <View style={styles.page}>
+    <ScrollView>
+<View style={styles.page}>
       <Header title="Sign Up" subTitle="Register and Eat" onBack={() => {}} />
       <View style={styles.container}>
         <View style={styles.photo}>
@@ -20,50 +30,67 @@ const SignUp = ({navigation}) => {
             </View>
           </View>
         </View>
-        <TextInput label="Full Name" placeholder="Type your full name" />
+        <TextInput
+          label="Full Name"
+          placeholder="Type your full name"
+          value={form.name}
+          onChangeText={value => setForm("name", value)}
+        />
         <Gap height={16} />
         <TextInput
           label="Email Address"
           placeholder="Type your email address"
+          value={form.email}
+          onChangeText={value => setForm("email", value)}
         />
         <Gap height={16} />
-        <TextInput label="Password" placeholder="Type your password" />
+        <TextInput
+          label="Password"
+          placeholder="Type your password"
+          value={form.password}
+          onChangeText={value => setForm("password", value)}
+        />
         <Gap height={24} />
-        <Button text="Continue" onPress={()=>navigation.navigate("SignUpAddress")} />
+        <Button
+          text="Continue"
+          onPress={onSubmit}
+        />
       </View>
     </View>
+    </ScrollView>
+    
   );
 };
 
 export default SignUp;
 
 const styles = StyleSheet.create({
-  page: {flex: 1},
+  page: { flex: 1 },
   container: {
-    backgroundColor: 'white',
+    backgroundColor: "white",
     paddingHorizontal: 24,
     paddingVertical: 26,
     marginTop: 24,
-    flex: 1,
+    flex: 1
   },
   photoContainer: {
     width: 90,
     height: 90,
     borderRadius: 90,
-    backgroundColor: '#F0F0F0',
-    padding: 24,
+    backgroundColor: "#F0F0F0",
+    padding: 24
   },
-  photo: {alignItems: "center",marginTop:26, marginBottom:16},
+  photo: { alignItems: "center", marginTop: 26, marginBottom: 16 },
   borderPhoto: {
     borderWidth: 1,
-    borderColor: '#8D92A3',
+    borderColor: "#8D92A3",
     width: 110,
     height: 110,
     borderRadius: 110,
-    borderStyle: 'dashed',
-    alignItems: 'center',
-    textAlign: 'center',
-    justifyContent: 'center',
+    borderStyle: "dashed",
+    alignItems: "center",
+    textAlign: "center",
+    justifyContent: "center"
   },
-  addPhoto: {fontSize: 14, fontFamily: 'Poppins-Light', color: '#8D92A3'},
+  addPhoto: { fontSize: 14, fontFamily: "Poppins-Light", color: "#8D92A3" }
 });
