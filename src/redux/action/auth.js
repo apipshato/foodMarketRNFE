dab43c687742b977714c786d071b25a5b21c0786
apipshato <afifshatos@gmail.com>
2021-07-08 13:35:35 +0700

    action creator redux part 2

--- src/redux/action/auth.js
+++ src/redux/action/auth.js
@@ -1,11 +1,13 @@
 import Axios from 'axios'
+import { showMessage } from '../../utils';
+import { setLoading } from './global';
 
 
 const API_HOST= {
     url:'http://foodmarket-backend.buildwithangga.id/api'
 }
 
-const signUpAction = (dataRegister) =>{
+const signUpAction = (dataRegister, photoReducer, navigation) =>(dispatch) =>{
     Axios.post(`${API_HOST.url}/register`, dataRegister)
     .then((res) => {
       console.log("data success: ", res.data);
