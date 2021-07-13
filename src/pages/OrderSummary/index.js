daf5eb578a5a142b6234e6dbb6dc42bac26b4515
apipshato <afifshatos@gmail.com>
2021-07-13 21:52:15 +0700

    checkout integration

--- src/pages/OrderSummary/index.js
+++ src/pages/OrderSummary/index.js
@@ -4,7 +4,8 @@ import { ScrollView, StyleSheet, Text, View } from "react-native";
 import { FoodDummy1 } from "../../assets";
 import { Button, Gap, Header, ItemListFood, ItemValue } from "../../components";
 
-const OrderSummary = ({navigation}) => {
+const OrderSummary = ({ navigation, route }) => {
+  const { item, transaction, userProfile } = route.params;
   return (
     <ScrollView>
       <Header
@@ -14,27 +15,39 @@ const OrderSummary = ({navigation}) => {
       />
       <View style={styles.content}>
         <Text style={styles.label}>OrderSummary</Text>
-        <ItemListFood type="order-summary" name="Sop Bumil" price="380.000" items={14} image={FoodDummy1} items={14}/>
+        <ItemListFood
+          type="order-summary"
+          name={item.name}
+          price={item.price}
+          items={{ uri :item.picturePath }}
+          image={FoodDummy1}
+          items={14}
+        />
         <Text style={styles.label}>Detail Transaction</Text>
-        <ItemValue label="Cherry Healty" value="IDR 18.390.000"/>
-        <ItemValue label="Driver" value="IDR 50.000"/>
-        <ItemValue label="Tax 10%" value="IDR 1.800.390"/>
-        <ItemValue label="Total Price" value="IDR 39.803.000" valueColor="#1ABC9C"/>
-     
-        
+        <ItemValue label="Cherry Healty" value="IDR 18.390.000" />
+        <ItemValue label="Driver" value="IDR 50.000" />
+        <ItemValue label="Tax 10%" value="IDR 1.800.390" />
+        <ItemValue
+          label="Total Price"
+          value="IDR 39.803.000"
+          valueColor="#1ABC9C"
+        />
       </View>
       <View style={styles.content}>
         <Text style={styles.label}>Deliver to:</Text>
-        <ItemValue label="Name" value="Afif Shato"/>
-        <ItemValue label="Phone No" value="089676645655"/>
-        <ItemValue label="Address" value="Jalan Danau Sentarum"/>
-        <ItemValue label="House No" value="jalur 1 no 5"/>
-        <ItemValue label="City" value="Pontianak"/>
+        <ItemValue label="Name" value="Afif Shato" />
+        <ItemValue label="Phone No" value="089676645655" />
+        <ItemValue label="Address" value="Jalan Danau Sentarum" />
+        <ItemValue label="House No" value="jalur 1 no 5" />
+        <ItemValue label="City" value="Pontianak" />
       </View>
       <View style={styles.button}>
-        <Button text="Checkout Now" onPress={() =>navigation.replace("SuccessOrder")}/>
+        <Button
+          text="Checkout Now"
+          onPress={() => navigation.replace("SuccessOrder")}
+        />
       </View>
-      <Gap height={40}/>
+      <Gap height={40} />
     </ScrollView>
   );
 };
@@ -42,20 +55,20 @@ const OrderSummary = ({navigation}) => {
 export default OrderSummary;
 
 const styles = StyleSheet.create({
-  content:{
-    backgroundColor:'white',
-    paddingHorizontal:24,
-    paddingVertical:16,
-    marginTop:24
+  content: {
+    backgroundColor: "white",
+    paddingHorizontal: 24,
+    paddingVertical: 16,
+    marginTop: 24
   },
-  label:{
-    fontSize:14,
-    fontFamily:"Poppins-Regular",
-    color:"#020202",
-    marginBottom:8
+  label: {
+    fontSize: 14,
+    fontFamily: "Poppins-Regular",
+    color: "#020202",
+    marginBottom: 8
   },
-  button:{
-    paddingHorizontal:24,
-    marginTop:24
+  button: {
+    paddingHorizontal: 24,
+    marginTop: 24
   }
 });
